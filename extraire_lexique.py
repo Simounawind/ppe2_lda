8b2c1ceb73fdf6d63f07426bed3e4fa789331911
Simounawind <1037674042@qq.com>
2023-03-08 14:32:21 +0100

    fusion des 3 fonctions, lexique fonction ajouté

--- extraire_lexique.py
+++ extraire_lexique.py
@@ -2,6 +2,7 @@ import os
 
 # Exo1-s1-r1
 
+
 def construit_liste_de_chaines():
     dir_path = './exercices/S1/Corpus/'
     list_contenu = []
@@ -10,6 +11,8 @@ def construit_liste_de_chaines():
             data = file.read()
             list_contenu.append(data)
     return list_contenu
+
+
 construit_liste_de_chaines()
 
 
@@ -26,7 +29,7 @@ def count_words(corpus_list):
     return dictionnaire_freq
 
 
-#Exo1-s1r3 
+# Exo1-s1r3
 def doc_freq(corpus):
     nb_doc = {}
     for doc in corpus:
@@ -37,3 +40,19 @@ def doc_freq(corpus):
             else:
                 nb_doc[word] = 1
     return nb_doc
+
+
+# pour afficher
+def lexique():
+    liste_gros = construit_liste_de_chaines()
+    dict_freq = count_words(liste_gros)
+    dict_occurence = doc_freq(liste_gros)
+    len_liste_final = len(dict_freq)
+    for i in range(len_liste_final):
+        key = list(dict_freq)[i]
+        value1 = list(dict_freq.values())[i]
+        value2 = list(dict_occurence.values())[i]
+        print("{0:^20}|{1:^20}|{2:^20}".format(key, value1, value2))
+
+
+lexique()
