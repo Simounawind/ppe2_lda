8c818fe046cdac0ccf015c937d9a7af028acd179
danyelkb <kanlibicakdanyel@gmail.com>
2023-03-16 16:07:29 +0100

    R3 fini

--- extraire_lexique.py
+++ extraire_lexique.py
@@ -1,5 +1,20 @@
 import os
 
+import sys
+
+
+# r3 , obtenir des données à partir de "ls"
+
+
+def r3():
+    list_contenu = []
+    for txtname in sys.stdin:
+        txtname = txtname.replace('\n', '')
+        with open(txtname, 'r') as file:
+            data = file.read()
+            list_contenu.append(data)
+    return list_contenu
+
 # Exo1-s1-r1
 
 
@@ -43,8 +58,10 @@ def doc_freq(corpus):
 
 
 # pour afficher
+
+
 def lexique():
-    liste_gros = construit_liste_de_chaines()
+    liste_gros = r3()
     dict_freq = count_words(liste_gros)
     dict_occurence = doc_freq(liste_gros)
     len_liste_final = len(dict_freq)
@@ -55,4 +72,21 @@ def lexique():
         print("{0:^20}|{1:^20}|{2:^20}".format(key, value1, value2))
 
 
-lexique()
+if __name__ == '__main__':
+    lexique()# pour afficher
+
+
+def lexique():
+    liste_gros = r3()
+    dict_freq = count_words(liste_gros)
+    dict_occurence = doc_freq(liste_gros)
+    len_liste_final = len(dict_freq)
+    for i in range(len_liste_final):
+        key = list(dict_freq)[i]
+        value1 = list(dict_freq.values())[i]
+        value2 = list(dict_occurence.values())[i]
+        print("{0:^20}|{1:^20}|{2:^20}".format(key, value1, value2))
+
+
+if __name__ == '__main__':
+    lexique()
