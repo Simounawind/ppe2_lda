e9d6dbda41b0f431b5f968685e5e75f45e08be65
Simounawind <1037674042@qq.com>
2023-03-29 11:58:14 +0200

    update_s7v1

--- work_class.py
+++ work_class.py
@@ -2,3 +2,10 @@ def gen():
     for i in [1, 2, 3, 4, 5]:
         yield i
     yield "fin"
+    return
+
+
+gen()
+
+
+time python pluri_extraire_xml.py 2022 - -date Dec/25 - -cat sport+culture+societe | wc - l
